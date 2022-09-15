20c9a15e7480ddb753e6693d44c0d2d203786da5
Kelvin Lucas <lucaskelvinbsantos@outlook.com>
2022-09-14 23:37:49 -0300

    Primeiro uso de JPA

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,32 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+		
+		
+	}
+
+}
